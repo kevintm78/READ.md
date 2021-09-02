5d517463a6cbf4ee18d21e4c1dfbadb6d5e2db6a
Kevin M <kevintm78@gmail.com>
2021-09-02 08:44:20 -0400

    repo script --possible wip
    Script provided by google source at https://gerrit.googlesource.com/git-repo/+/refs/heads/master/README.md
    
    I made **very minor alterations** that's not been tested. Probably still needs to be touched up or maybe even touched down.

---
+++ repo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#---------------------#
+# Repo install script #
+#---------------------#
+mkdir -p ~/.bin
+PATH="${HOME}/.bin:${PATH}"
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
+chmod a+rx ~/.bin/repo
